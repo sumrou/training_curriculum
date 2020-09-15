13e20c778f2e2e6ca0929757858924961e75f149
sumrou <69175299+sumrou@users.noreply.github.com>
2020-09-15 19:17:19 +0900

    Issue6直し

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,7 +34,7 @@ class CalendarsController < ApplicationController
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      wday_num = Date.today.wday + x
+      wday_num = @todays_date.wday + x
       if wday_num.to_i >= 7 
         wday_num = wday_num - 7
       end
